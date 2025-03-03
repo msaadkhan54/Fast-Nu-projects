bd726439fe9885f5abfb15df98dea95afee187f9
Muhammad Saad Khan <apexsaad54@gmail.com>
2025-03-03 15:51:18 +0500

    Delete Lab 5 directory

--- Lab 5/lab5.Q3.prob2.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int num;
-	printf("Enter number");
-	scanf("%d", &num);
-	(num>0)?printf("The %d is positive",num):(num<0)?printf("The %d is negative",num):printf("The %d is zero.",num);
-	return 0;
-}
