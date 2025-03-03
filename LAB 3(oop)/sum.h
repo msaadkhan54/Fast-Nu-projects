3c284548880bffa022641975a242dafa18613987
Muhammad Saad Khan <apexsaad54@gmail.com>
2025-03-03 15:56:23 +0500

    Delete LAB 3(oop) directory

--- LAB 3(oop)/sum.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef sum_h
-#define sum_h
-int sum(int a,int b){
-	return a+b;
-}
-
-#endif
--- LAB 3(oop)/sum1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include"sum.h"
-using namespace std;
-
-int sumTwice(int a,int b){
-	return sum(a,b)*2;
-}
